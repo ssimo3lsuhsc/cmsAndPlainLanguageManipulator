85cd524de44b4b178f1d15d774dbe3ea1333cfc2
Stuart Simon <ssimo3@lsuhsc.edu>
2023-03-23 15:45:52 -0500

    Revert "Updated scratch.py to not prettify html file (could cause spacing issues in output)"
    This reverts commit 1913e233fce6dc8aa3f54da982d8655a295f1f00.

--- scratch.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("scratch.html") as scratch:
-    soup = BeautifulSoup(scratch, "html.parser")
-    #Do stuff here
-
-    with open("output.html", "w") as output:
-        output.truncate(0)
-        output.write(str(soup))
