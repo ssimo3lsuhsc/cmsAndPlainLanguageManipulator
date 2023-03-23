1913e233fce6dc8aa3f54da982d8655a295f1f00
Stuart Simon <ssimo3@lsuhsc.edu>
2023-03-23 15:42:41 -0500

    Updated scratch.py to not prettify html file (could cause spacing issues in output)

---
+++ scratch.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+with open("scratch.html") as scratch:
+    soup = BeautifulSoup(scratch, "html.parser")
+    #Do stuff here
+
+    with open("output.html", "w") as output:
+        output.truncate(0)
+        output.write(str(soup))
